250ca65338180f0ec0147105f9f70190e0242661
happy_ryo <iwama.ryo.0731@gmail.com>
2022-11-15 09:08:17 +0900

    added file.

---
+++ vpc/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "gamers" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc-gamers"
+  }
+}
+
+resource "aws_subne" "gamers" {
+  vpc_id = aws_vpc.gamers.id
+
+  cidr_block = "10.0.1.0/24"
+}
+
